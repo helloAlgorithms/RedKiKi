4c7c23fc102f2a366cd93abb2d7997d5d92d4c11
sohan <sohan@student.42seoul.kr>
2022-12-20 18:22:18 +0900

    FEAT : send result to slack channel

--- src/SlackAPI.py
+++ src/SlackAPI.py
@@ -40,13 +40,9 @@ token = os.environ.get("SLACK_BOT_USER_OAUTH_TOKEN")
 slack = SlackAPI(token)
 
 channel_name = "vacation-notice"
-query = "슬랙 봇 테스트"
-text = "자동 생성 문구 테스트"
 
-# 채널ID 파싱
 channel_id = slack.get_channel_id(channel_name)
 os.system("git clone https://github.com/helloAlgorithms/helloAlgorithms.git")
-print("============================")
 raw_list = list(map(str, os.popen("cd helloAlgorithms && git branch -r")))
 users = []
 for raw in raw_list[1::]:
@@ -54,17 +50,51 @@ for raw in raw_list[1::]:
      users.append(user)
 users.remove("master")
 users.remove("test")
-print(users)
-print("============================")
-users_commit_count = {}
+
+vacation_users = {}
+pass_users = {}
 for user in users:
     os.system("cd helloAlgorithms && git checkout {user}".format(user = user))
-    commit_count = os.popen("cd helloAlgorithms && \
+    raw_commit_count = os.popen("cd helloAlgorithms && \
             git log --since=2.weeks --pretty=format:{git_log_format} \
             --grep {bracket} | sort -k 3 | wc -l".format(user = user, git_log_format = '"%cr : %s"',bracket='"\["'))
-    users_commit_count[user] = commit_count
+    commit_count = raw_commit_count.read()
+    commit_count.strip()
+    commit_count = int(commit_count)
+    if commit_count < 8:
+        vacation_users[user] = commit_count
+    else:
+        pass_users[user] = commit_count
 
-for key,value in users_commit_count.items():
-    print(key, ":", *value)
-slack.post_thread(channel_id, text)
+vacation_users = sorted(vacation_users.items(), key = lambda x:x[1])
+pass_users = sorted(pass_users.items(), key = lambda x:x[1], reverse = True)
+
+vacation_text = ":cry: vacation members :cry:\n"
+pass_text = ":laughing: passed members :laughing:\n"
+
+for index, user in enumerate(vacation_users):
+    text = f":desert_island:`{user[0]}`"
+    width = 11 - len(user[0])
+    text +=  " " * width
+    text += f"{user[1]}\t"
+    if (index + 1) % 4 == 0:
+        text += "\n"
+    vacation_text += text 
+if len(vacation_users) == 0:
+    vacation_text += "No one goes to vacation!!:partying_face:\n"
+
+for index, user in enumerate(pass_users):
+    text = f":fire:`{user[0]}`"
+    width = 11 - len(user[0])
+    text +=  " " * width
+    text += f"{user[1]}\t"
+    if (index + 1) % 4 == 0:
+        text += "\n"
+    pass_text += text
+if len(pass_users) == 0:
+    pass_text += "Doomsday :skull_and_crossbones:\n"
+
+slack.post_thread(channel_id, vacation_text)
+slack.post_thread(channel_id, pass_text)
+
 os.system("rm -rf helloAlgorithms")
